7833664ced9b4835b793829c41e90f46681e8697
Muthuvel Sivaraman <muthuvel.sivaraman@gmail.com>
2025-05-07 16:41:09 +0530

    Create pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 function App() {
   return (
    
-    <h1 className='text-2xl bg-red-400'> helloooo Universe</h1>
+<>
+<Router>
+  <Routes>
+    <Route path="/" element={<Home/>} > </Route> 
+    <Route path="/profile" element={<Profile/>} > </Route> 
+    <Route path="/Sign-In" element={<SignIn/>} > </Route> 
+    <Route path="/Sign-Up" element={<SignUp/>} > </Route> 
+    <Route path="/offers" element={<Offers/>} > </Route> 
+    <Route path="/Forgot-Password" element={<ForgotPassword/>} > </Route> 
+  </Routes>
+</Router>
+
+</>
+
   );
 }
 
 export default App;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    console.log("home");
+    return (
+         
+    <div>
+      <h1> Hello  HOme </h1>
+    </div>
+  )
+}
